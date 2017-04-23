5f663e195951b2693b15b63e4854a804bb18e58b
haoyifen <526667233@qq.com>
2017-04-23 21:58:47 +0800

    test

---
+++ MyApp/src/main/java/MyApp.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by haoyifen on 2017/4/23 21:57.
+ */
+public class MyApp {
+	public static void main(String[] args) {
+		new SpringApplication().run(args);
+	}
+}
